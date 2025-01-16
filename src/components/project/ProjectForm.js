ebb1a08dd044292ebd4f0aa559fd0ddde9e7d3aa
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-16 10:48:47 +0200

    Conectando a APP, a API usando o modúlo, json-server

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,8 +1,31 @@
+import { useState, useEffect, useRef   } from 'react';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css'
 function ProjectForm( {btnText}){
+
+   const [categorias, setCategorias] = useState([])
+   const hasFetched = useRef(false); // Variável para verificar se já fez a requisição
+
+
+   useEffect(() => {
+      if (!hasFetched.current) {
+        hasFetched.current = true; // Marca como já feita
+        fetch('http://localhost:5000/categorias', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setCategorias(data);
+          })
+          .catch((error) => console.error('Error:', error));
+      }
+    }, []);
+
     return (
      <form className={styles.form}>
         <Input
@@ -14,10 +37,14 @@ function ProjectForm( {btnText}){
         <Input
          type="number"
          text="Orçamento do Projecto"
-         name="name"
+         name="budget"
          placeholder="Digite o orçamento total"
         />
-       <Select name="categoria_id" text="Selecione a categoria" />
+       <Select 
+         name="categoria_id" 
+         text="Selecione a categoria" 
+         options={categorias} 
+         />
        <SubmitButton text={btnText}/>
      </form>
     )
