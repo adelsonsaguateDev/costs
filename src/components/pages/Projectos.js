0fed798cc0f88b40da0a82ca2f03ed8a211c25ae
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-24 20:00:43 +0200

    Estilizando a pagina de projectos

--- src/components/pages/Projectos.js
+++ src/components/pages/Projectos.js
@@ -1,21 +1,30 @@
-import { useLocation } from "react-router-dom"
-import Message from "../layout/Message"
-function Projectos(){
-   
-   const location = useLocation()
-   let message = ''
+import { useLocation } from "react-router-dom";
+import Message from "../layout/Message";
+import styles from "./Projectos.module.css";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
-   if(location.state){
-      message = location.state.message
-   }
+function Projectos() {
+  const location = useLocation();
+  let message = "";
 
-   return (
-      <div>
-         <h1>Meus Projectos</h1>
-         {message && <Message type="success" msg={message} /> }
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div className={styles.project_container} >
+      <div className={styles.title_container}>
+        <h1>Meus Projectos</h1>
+        <LinkButton to="/novoprojecto" text="Criar Projecto" />
       </div>
-   )
+
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projectos...</p>
+      </Container>
+    </div>
+  );
 }
 
-
-export default Projectos
+export default Projectos;
