3e531a8611c95c0bf2a9d1ee07ba6db8e44df7bf
Adelson Saguate <adelsonsaguate@acsun-mz.com>
2025-01-30 19:39:07 +0200

    Implementando o Loader nas lista de projectos

--- src/components/pages/Projectos.js
+++ src/components/pages/Projectos.js
@@ -1,13 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
-import styles from "./Projectos.module.css";
 import Container from "../layout/Container";
+import Loading from "../layout/Loading";
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
 
+import styles from "./Projectos.module.css";
 function Projectos() {
   const [projects, setProjects] = useState([]);
+  const [hideLoader, setHideLoader] = useState(false);
 
   const location = useLocation();
   let message = "";
@@ -21,18 +23,20 @@ function Projectos() {
   useEffect(() => {
     if (!hasFetched.current) {
       hasFetched.current = true; // Marca como já feita
-      fetch("http://localhost:5000/projectos", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setProjects(data);
-          console.log(data);
+      setTimeout(() => { //Usado para fazer o loader levar mais tempo
+        fetch("http://localhost:5000/projectos", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         })
-        .catch((error) => console.log(error)());
+          .then((response) => response.json())
+          .then((data) => {
+            setProjects(data);
+            setHideLoader(true);
+          })
+          .catch((error) => console.log(error)());
+      }, 2000);
     }
   }, []);
 
@@ -49,15 +53,19 @@ function Projectos() {
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
         {projects.length > 0 &&
-          projects.map((project) => 
-          <ProjectCard 
-          id={project.id} 
-          name={project.name} 
-          budget={project.budget} 
-          category={project.categoria?.nome || "Sem Categoria"} 
-          key={project.id} 
-
-          />)}
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.categoria?.nome || "Sem Categoria"}
+              key={project.id}
+            />
+          ))}
+        {!hideLoader && <Loading />}
+        {hideLoader &&  projects.length === 0 && (
+          <p>Nenhum projecto encontrado.</p>
+        )}
       </Container>
     </div>
   );
